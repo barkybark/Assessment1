0c0b31ff4e3bb10b716efe280beef9870232c5b9
barkybark <egao02hee@gmail.com>
2025-08-18 20:28:55 +0900

    dd

--- Assessment1.py
+++ Assessment1.py
@@ -73,7 +73,7 @@ def main():
             st.session_state.mode = "study"
 
 # daily_mode 유지
-    if "mode" in st.session_state.mode == "daily":
+    if st.session_state.mode == "daily":
         st.subheader("📅 오늘의 문제")
         # 책 내용 기반으로 GPT가 문제 생성
         if "daily_question" not in st.session_state:
@@ -176,7 +176,7 @@ def main():
     #             feedback = ask_gpt(eval_prompt)
     #             st.markdown(feedback)
 
-    if "mode" in st.session_state and st.session_state.mode == "study":
+    if  st.session_state.mode == "study":
         st.subheader("📚 공부 모드 시작")
 
         # 1️⃣ 대화 상태 초기화
