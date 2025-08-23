ed3fa551b169c6ff5a1652822b5b5c0f23002088
barkybark <egao02hee@gmail.com>
2025-08-23 19:39:35 +0900

    sdeby

--- Assessment1.py
+++ Assessment1.py
@@ -182,9 +182,7 @@ def main():
             # 문제와 답변 유지 (필요하면 제거 가능)
             st.session_state.daily_answer = user_answer
 
-
-
-            if st.button("제출"):
+            if st.button("제출") and st.session_state.mode=="daily":
                 st.write(st.session_state.mode)
                 st.write("제출버튼클릭완료")
                 
